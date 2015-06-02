c4cb77f99fef7cde3bc7f3ad672dc70ee40ba773
fiamma-right <ariel.manriquez.14@sansano.usm.cl>
2015-06-01 23:15:54 -0400

    Update and rename tarea_3.c to tarea_3.cpp

---
+++ tarea_3.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stlib.h>
+
+typedef struct tnode{
+  struct right;
+  struct next;
+  float val;
+  char[8];
+}Node;
+
+Tnode BuildTree(int *InOrder, int *PostOrden);
+
+int main(){
+  
+}
+
+Node BuildTree(int *InOrder, int *PostOrden){
+  return Node Treebuilding()
+}
+
+
