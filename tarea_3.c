54ccb7ee1323a4d21c5cd1782f745758531c2a8e
fiamma-right <ariel.manriquez.14@sansano.usm.cl>
2015-06-02 02:19:11 -0400

    Update tarea_3.c

--- tarea_3.c
+++ tarea_3.c
@@ -3,9 +3,9 @@
 
 typedef struct tnode{
   Node right;
-  struct next; // arreglar struct
+  Node left; 
   float val;
-  char nombre[8];
+  int nombre;
 }Node;
 
 
@@ -16,44 +16,64 @@ int main(){
   return 0;
 }
 
-Node BuildTree(int *InOrder, int *PostOrder, int size){
+Node BuildTree(float *InOrder,int *inorderName, float *PostOrder,int *postorderName, int size){
   int instart = 0;
   int inend = size-1;
   int poststart = 0;
   int postend = size -1;
-  return Node Treebuilding(InOrder, instart, inend, PostOrder, poststart, postend);
+  return Node Treebuilding(InOrder, inorderName, instart, inend, PostOrder, postorderName, poststart, postend);
 }
 
-Node Treebuilding(int *InOrder, int instart, int inend,int PostOrder,int poststart,int  postend , ){ 
+Node Treebuilding(float *InOrder,int *inorderName, float instart, int inend,int PostOrder, int *postorderName, int poststart,int  postend , ){ 
   if (instart > inend|| poststart > postend)
     return NULL;
   
   
-  int rootvalue = PostOrder[postend];
+  float rootvalue = PostOrder[postend];
+  int rootname = postorderName[postend];
   Node *root = (Node*)malloc(sizeof(Node)); // Falta hacer que agregue el nombre del liceo al arbol
-  root.val    = rootvalue;
-  root.nombre =  ;
+  root->val    = rootvalue;
+  root->nombre = rootname;
   int k =0;
  
   for (int i=0; i < inend; i++){
-    if(InOrder[i] == rootvalue){
+    if(inorderName[i] == rootname){
       k=i;
       break;
     }
   }
   
-  root.left = Treebuilding (InOrder, instart, k-1, PostOrder, poststart, poststart+k-(instart+1));
-  root.right= Treebuilding (InOrder, k+1, inend, PostOrder, poststart+k-instart, postend);
+  root->left = Treebuilding (InOrder, instart, k-1, PostOrder, poststart, poststart+k-(instart+1));
+  root->right= Treebuilding (InOrder, k+1, inend, PostOrder, poststart+k-instart, postend);
   
   return root;
 }
 
-Node Cargar_arbol(char *nombre){
-  FILE* fp; int size;
+Node *Cargar_arbol(char *nombre){
+  FILE* fp; int size, *inorderName, *postorderName;
+  float *inorder, *postorder;
+  char aux[8];
+  Node *Root;
   fp = fopen(nombre, "r");
   fscanf(fp, "%d", &size);
+  inorder = (float*)malloc(sizeof(float)*size);
+  inorderName = (int*)malloc(sizeof(int)*size);
+  postorder = (float*)malloc(sizeof(float)*size);
+  postorderName = (int*)malloc(sizeof(int)*size);
   for(int i=0; i<size; i++){
-    
+    fscanf(fp, "Liceo %d | %f", inorderName[i],  inorder[i]);
+  }
+  for(int i=0; i<size; i++){
+    fscanf(fp, "Liceo %d | %f", postorderName[i],  postorder[i]);
+  }
+Root = BuildTree(inorder, inorderName, postorder, postorderName);
+return root;
+  
+}
+
+Node *Borrar_nodo(int nom, Node *root){
+  while(root->nombre != nom){
+    if(root->)
   }
 }
 
