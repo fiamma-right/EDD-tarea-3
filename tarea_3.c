af7639d3cab7a621a8e9366599a8306ef6a72f5b
fiamma-right <ariel.manriquez.14@sansano.usm.cl>
2015-06-12 02:07:03 -0400

    Update tarea_3.c

--- tarea_3.c
+++ tarea_3.c
@@ -9,10 +9,14 @@ typedef struct tnode{
 }Node;
 
 
-Node BuildTree(int *InOrder, int *PostOrden);
-Node Treebuilding(int *InOrder, int instart, int inend,int PostOrder,int poststart,int  postend );
+Node BuildTree( int *InOrder, int *PostOrden);
+Node Treebuilding( int *InOrder, int instart, int inend,int PostOrder,int poststart,int  postend );
+void insert( struct btreenode **, int );
+void delete( struct btreenode **, int );
+void search( struct btreenode **, int, struct btreenode **, struct btreenode **, int * );
 
 int main(){
+  
   return 0;
 }
 
@@ -31,7 +35,7 @@ Node Treebuilding(float *InOrder,int *inorderName, float instart, int inend,int 
   
   float rootvalue = PostOrder[postend];
   int rootname = postorderName[postend];
-  Node *root = (Node*)malloc(sizeof(Node)); // Falta hacer que agregue el nombre del liceo al arbol
+  Node *root = (Node*)malloc(sizeof(Node)); 
   root->val    = rootvalue;
   root->nombre = rootname;
   int k =0;
@@ -71,9 +75,131 @@ return root;
   
 }
 
-Node *Borrar_nodo(int nom, Node *root){
-  while(root->nombre != nom){
-    if(root->)
-  }
+void delete ( struct btreenode **root, int num )
+{
+    int found ;
+    struct btreenode *parent, *x, *xsucc ;
+
+    /* if tree is empty */
+if ( *root == NULL )
+    {
+        printf ( "\nTree is empty" ) ;
+        return ;
+    }
+
+    parent = x = NULL ;
+
+    /* call to search function to find the node to be deleted */
+
+    search ( root, num, &parent, &x, &found ) ;
+
+    /* if the node to deleted is not found */
+if ( found == FALSE )
+    {
+        printf ( "\nData to be deleted, not found" ) ;
+        return ;
+    }
+
+    /* if the node to be deleted has two children */
+if ( x -> leftchild != NULL && x -> rightchild != NULL )
+    {
+        parent = x ;
+        xsucc = x -> rightchild ;
+
+        while ( xsucc -> leftchild != NULL )
+        {
+            parent = xsucc ;
+            xsucc = xsucc -> leftchild ;
+        }
+
+        x -> data = xsucc -> data ;
+        x = xsucc ;
+    }
+
+    /* if the node to be deleted has no child */
+if ( x -> leftchild == NULL && x -> rightchild == NULL )
+    {
+        if ( parent -> rightchild == x )
+            parent -> rightchild = NULL ;
+        else
+            parent -> leftchild = NULL ;
+
+        free ( x ) ;
+        return ;
+    }
+
+    /* if the node to be deleted has only rightchild */
+if ( x -> leftchild == NULL && x -> rightchild != NULL )
+    {
+        if ( parent -> leftchild == x )
+            parent -> leftchild = x -> rightchild ;
+        else
+            parent -> rightchild = x -> rightchild ;
+
+        free ( x ) ;
+        return ;
+    }
+
+    /* if the node to be deleted has only left child */
+if ( x -> leftchild != NULL && x -> rightchild == NULL )
+    {
+        if ( parent -> leftchild == x )
+            parent -> leftchild = x -> leftchild ;
+        else
+            parent -> rightchild = x -> leftchild ;
+
+        free ( x ) ;
+        return ;
+    }
+}
 }
 
+void insert ( struct btreenode **sr, int num )
+{
+    if ( *sr == NULL )
+    {
+        *sr = malloc ( sizeof ( struct btreenode ) ) ;
+
+        ( *sr ) -> leftchild = NULL ;
+        ( *sr ) -> data = num ;
+        ( *sr ) -> rightchild = NULL ;
+    }
+    else/* search the node to which new node will be attached */
+
+    {
+        /* if new data is less, traverse to left */
+if ( num < ( *sr ) -> data )
+            insert ( &( ( *sr ) -> leftchild ), num ) ;
+        else/* else traverse to right */
+
+            insert ( &( ( *sr ) -> rightchild ), num ) ;
+    }
+}
+
+void search ( struct btreenode **root, int num, struct btreenode **par, struct
+        btreenode **x, int *found )
+{
+    struct btreenode *q ;
+
+    q = *root ;
+    *found = FALSE ;
+    *par = NULL ;
+
+    while ( q != NULL )
+    {
+        /* if the node to be deleted is found */
+if ( q -> data == num )
+        {
+            *found = TRUE ;
+            *x = q ;
+            return ;
+        }
+
+        *par = q ;
+
+        if ( q -> data > num )
+            q = q -> leftchild ;
+        else
+            q = q -> rightchild ;
+    }
+}
