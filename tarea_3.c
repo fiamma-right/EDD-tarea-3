c59c1fe36fb00d48ea2c0c3f43d08120629f67e9
fiamma-right <ariel.manriquez.14@sansano.usm.cl>
2015-06-13 00:17:16 +0000

    Update tarea_3.c

--- tarea_3.c
+++ tarea_3.c
@@ -1,76 +1,80 @@
 #include <stdio.h>
-#include <stlib.h>
+#include <stdlib.h>
 
-typedef struct tnode{
-  Node right;
-  Node left; 
-  float val;
-  int nombre;
+#define FALSE 0
+#define TRUE 1
+
+typedef struct btreenode{
+  	struct btreenode *rightchild;
+  	struct btreenode *leftchild; 
+  	float val;
+  	int data;
 }Node;
 
 
-Node BuildTree( int *InOrder, int *PostOrden);
-Node Treebuilding( int *InOrder, int instart, int inend,int PostOrder,int poststart,int  postend );
+struct btreenode BuildTree( float *,int *, float *, int *, int);
+struct btreenode Treebuilding(float *, int *, int , int, float *, int *, int, int);
 void insert( struct btreenode **, int );
 void delete( struct btreenode **, int );
 void search( struct btreenode **, int, struct btreenode **, struct btreenode **, int * );
 
 int main(){
-  
-  return 0;
+	FILE* fp;
+	char name[100], cmd[30], aux[10];
+	while(
+	  
+	return 0;
 }
 
-Node BuildTree(float *InOrder,int *inorderName, float *PostOrder,int *postorderName, int size){
-  int instart = 0;
-  int inend = size-1;
-  int poststart = 0;
-  int postend = size -1;
-  return Node Treebuilding(InOrder, inorderName, instart, inend, PostOrder, postorderName, poststart, postend);
+struct btreenode BuildTree(float *InOrder,int *inorderName, float *PostOrder,int *postorderName, int size){
+  	int instart = 0;
+  	int inend = size-1;
+  	int poststart = 0;
+  	int postend = size -1;
+  	return Treebuilding(InOrder, inorderName, instart, inend, PostOrder, postorderName, poststart, postend);
 }
 
-Node Treebuilding(float *InOrder,int *inorderName, float instart, int inend,int PostOrder, int *postorderName, int poststart,int  postend , ){ 
-  if (instart > inend|| poststart > postend)
-    return NULL;
-  
-  
-  float rootvalue = PostOrder[postend];
-  int rootname = postorderName[postend];
-  Node *root = (Node*)malloc(sizeof(Node)); 
-  root->val    = rootvalue;
-  root->nombre = rootname;
-  int k =0;
+struct btreenode Treebuilding(float *InOrder,int *inorderName, int instart, int inend,float *PostOrder, int *postorderName, int poststart,int  postend){
+	int i; 
+	if (instart > inend|| poststart > postend)
+		return ;
+	float rootvalue = PostOrder[postend];
+	int rootname = postorderName[postend];
+	struct btreenode *root = (struct btreenode*)malloc(sizeof(struct btreenode)); 
+	root->val    = rootvalue;
+	root->data = rootname;
+	int k =0;
  
-  for (int i=0; i < inend; i++){
-    if(inorderName[i] == rootname){
-      k=i;
-      break;
+	for (i=0; i < inend; i++){
+		if(inorderName[i] == rootname){
+		k=i;
+      		break;
     }
   }
+
+	*root->leftchild = Treebuilding (InOrder, inorderName, instart, k-1, PostOrder,postorderName, poststart, poststart+k-(instart+1));
+	*root->rightchild= Treebuilding (InOrder,inorderName, k+1, inend, PostOrder,postorderName, poststart+k-instart, postend);
   
-  root->left = Treebuilding (InOrder, instart, k-1, PostOrder, poststart, poststart+k-(instart+1));
-  root->right= Treebuilding (InOrder, k+1, inend, PostOrder, poststart+k-instart, postend);
-  
-  return root;
+  return *root;
 }
 
-Node *Cargar_arbol(char *nombre){
-  FILE* fp; int size, *inorderName, *postorderName;
-  float *inorder, *postorder;
-  char aux[8];
-  Node *Root;
-  fp = fopen(nombre, "r");
-  fscanf(fp, "%d", &size);
-  inorder = (float*)malloc(sizeof(float)*size);
-  inorderName = (int*)malloc(sizeof(int)*size);
-  postorder = (float*)malloc(sizeof(float)*size);
-  postorderName = (int*)malloc(sizeof(int)*size);
-  for(int i=0; i<size; i++){
-    fscanf(fp, "Liceo %d | %f", inorderName[i],  inorder[i]);
-  }
-  for(int i=0; i<size; i++){
-    fscanf(fp, "Liceo %d | %f", postorderName[i],  postorder[i]);
-  }
-Root = BuildTree(inorder, inorderName, postorder, postorderName);
+struct btreenode *Cargar_arbol(char *nombre){
+	FILE* fp; int size, *inorderName, *postorderName;
+	float *inorder, *postorder; int i;
+	struct btreenode *root;
+	fp = fopen(nombre, "r");
+	fscanf(fp, "%d", &size);
+	inorder = (float*)malloc(sizeof(float)*size);
+	inorderName = (int*)malloc(sizeof(int)*size);
+	postorder = (float*)malloc(sizeof(float)*size);
+	postorderName = (int*)malloc(sizeof(int)*size);
+	for(i=0; i<size; i++){
+		fscanf(fp, "Liceo %d | %f", &inorderName[i],  &inorder[i]);
+	}
+	for(i=0; i<size; i++){
+		fscanf(fp, "Liceo %d | %f", &postorderName[i],  &postorder[i]);
+  	}
+	*root = BuildTree(inorder, inorderName, postorder, postorderName, size);
 return root;
   
 }
@@ -81,8 +85,7 @@ void delete ( struct btreenode **root, int num )
     struct btreenode *parent, *x, *xsucc ;
 
     /* if tree is empty */
-if ( *root == NULL )
-    {
+if ( *root == NULL ){
         printf ( "\nTree is empty" ) ;
         return ;
     }
@@ -94,20 +97,17 @@ if ( *root == NULL )
     search ( root, num, &parent, &x, &found ) ;
 
     /* if the node to deleted is not found */
-if ( found == FALSE )
-    {
+if ( found == FALSE ){
         printf ( "\nData to be deleted, not found" ) ;
         return ;
     }
 
     /* if the node to be deleted has two children */
-if ( x -> leftchild != NULL && x -> rightchild != NULL )
-    {
+if ( x -> leftchild != NULL && x -> rightchild != NULL ){
         parent = x ;
-        xsucc = x -> rightchild ;
+        xsucc = x -> rightchild;
 
-        while ( xsucc -> leftchild != NULL )
-        {
+        while ( xsucc -> leftchild != NULL ){
             parent = xsucc ;
             xsucc = xsucc -> leftchild ;
         }
@@ -117,8 +117,7 @@ if ( x -> leftchild != NULL && x -> rightchild != NULL )
     }
 
     /* if the node to be deleted has no child */
-if ( x -> leftchild == NULL && x -> rightchild == NULL )
-    {
+if ( x -> leftchild == NULL && x -> rightchild == NULL ){
         if ( parent -> rightchild == x )
             parent -> rightchild = NULL ;
         else
@@ -129,8 +128,7 @@ if ( x -> leftchild == NULL && x -> rightchild == NULL )
     }
 
     /* if the node to be deleted has only rightchild */
-if ( x -> leftchild == NULL && x -> rightchild != NULL )
-    {
+if ( x -> leftchild == NULL && x -> rightchild != NULL ){
         if ( parent -> leftchild == x )
             parent -> leftchild = x -> rightchild ;
         else
@@ -141,8 +139,7 @@ if ( x -> leftchild == NULL && x -> rightchild != NULL )
     }
 
     /* if the node to be deleted has only left child */
-if ( x -> leftchild != NULL && x -> rightchild == NULL )
-    {
+if ( x -> leftchild != NULL && x -> rightchild == NULL ){
         if ( parent -> leftchild == x )
             parent -> leftchild = x -> leftchild ;
         else
@@ -151,7 +148,6 @@ if ( x -> leftchild != NULL && x -> rightchild == NULL )
         free ( x ) ;
         return ;
     }
-}
 }
 
 void insert ( struct btreenode **sr, int num )
